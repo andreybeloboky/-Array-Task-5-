8925b49d4a5b0e0f9b1e8d462cb0eac955a04435
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-07-25 18:52:43 +0300

    add a separate class for displaying the array to the screen

---
+++ src/com/company/ArrayOutput.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class ArrayOutput {
+    public static void consoleOutput(int[] sortingCombArray) {
+        for (int i = 0; i<sortingCombArray.length; i++){
+            System.out.println(sortingCombArray[i]);
+        }
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size array");
         int size = scanner.nextInt();
         if (size > 0) {
             int[] array = ArrayGenerator.createArray(size);
             int[] sortingCombArray = SortingComb.sortingArray(array);
-            for (int i = 0; i<array.length; i++){
-                System.out.println(sortingCombArray[i]);
-            }
+            ArrayOutput.consoleOutput(sortingCombArray);
         }else {
             System.out.println("The size of the array must be a positive number ");
         }
