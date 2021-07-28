bd0132b93c4fb9cbb498b2198b3dbb4dae95d883
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-07-28 12:18:47 +0300

    add javadoc in ArrayOutput class

--- src/com/company/ArrayOutput.java
+++ src/com/company/ArrayOutput.java
@@ -1,5 +1,8 @@
 package com.company;
 
+/*
+ * @param sortingCombArray - array sorted in order ;
+ */
 public class ArrayOutput {
     public static void consoleOutput(int[] sortingCombArray) {
         for (int i = 0; i < sortingCombArray.length; i++) {
